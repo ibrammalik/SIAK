38b263898444d0fe0af6a529d9b0bc16213009d2
ibrammalik <malikmaul1173@gmail.com>
2025-12-03 13:16:14 +0700

    self host assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,11 @@
 
     <title>{{ $title }} - SIAK {{ $nama_kelurahan }}</title>
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
-
     <!-- Styles / Scripts -->
+    <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/brands.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/solid.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/fonts.css') }}" rel="stylesheet" >
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
     @livewireStyles
